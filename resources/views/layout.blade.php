0290a8c74a0c4e8795c4b16494941f7c9c223210
xJakub <tortillaconalioli@gmail.com>
2018-03-12 20:46:52 +0100

    Mantain query in search box

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,7 +32,7 @@
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0" action="{{ route('nendoroids-search') }}">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="q">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="q" value="{{ Request::get('q') }}">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
